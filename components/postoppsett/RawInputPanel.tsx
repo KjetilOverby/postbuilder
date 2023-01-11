ef43862b5f7857983905c3fd1aafafa09fc701f3
KjetilOverby <kjeoever@msn.com>
2023-01-11 07:06:18 +0100

    key

--- components/postoppsett/RawInputPanel.tsx
+++ components/postoppsett/RawInputPanel.tsx
@@ -1,110 +1,109 @@
-import React from 'react'
+import React from "react";
 
+const RawInputPanel = ({ raw }: any) => {
+  return (
+    <>
+      <div>
+        <table>
+          <tbody>
+            <tr>
+              <th className="nominell">nom</th>
+              <th className="nom18">18%</th>
+              <th className="nom12">12%</th>
+            </tr>
+            <tr>
+              <td>
+                {raw.map((item: any) => {
+                  return (
+                    <p key={item.id} className="nom nominell">
+                      {item.nom}
+                    </p>
+                  );
+                })}
+              </td>
+              <td>
+                {raw.map((item: any) => {
+                  return (
+                    <p
+                      /* onClick={getRawRings} */
+                      key={item.id}
+                      className="nom nom18"
+                    >
+                      {item.r18}
+                    </p>
+                  );
+                })}
+              </td>
+              <td>
+                {raw.map((item: any) => {
+                  return (
+                    <p
+                      /* onClick={getRawRings} */
+                      key={item.id}
+                      className="nom nom12"
+                    >
+                      {item.r12}
+                    </p>
+                  );
+                })}
+              </td>
+              <td>
+                {raw.map((item: any) => {
+                  return (
+                    <p
+                      /* onClick={getRawRings} */
+                      key={item.id}
+                      className="nom nom12"
+                    >
+                      {item.r12s}
+                    </p>
+                  );
+                })}
+              </td>
+              <td>
+                {raw.map((item: any) => {
+                  return (
+                    <p
+                      /* onClick={getRawRings} */
+                      key={item.id}
+                      className="nom nom12"
+                    >
+                      {item.r12s1}
+                    </p>
+                  );
+                })}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <style jsx>
+        {`
+          .container {
+          }
+          .nom {
+            color: pink;
+          }
+          .nom:hover {
+            cursor: pointer;
+          }
+          .nominell {
+            color: indianred;
+          }
+          .nom12 {
+            color: red;
+          }
+          .nom18 {
+            color: seagreen;
+          }
+          td {
+            border: 0.5px solid #333;
+            padding: 2px;
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
-const RawInputPanel = ({raw}) => {
-return (
-<>
-<div>
-<table>
-              <tbody>
-                <tr>
-                  <th className="nominell">nom</th>
-                  <th className="nom18">18%</th>
-                  <th className="nom12">12%</th>
-                </tr>
-                <tr>
-                  <td>
-                    {raw.map((item) => {
-                      return (
-                        <p key={item.id} className="nom nominell">
-                          {item.nom}
-                        </p>
-                      );
-                    })}
-                  </td>
-                  <td>
-                    {raw.map((item) => {
-                      return (
-                        <p
-                          /* onClick={getRawRings} */
-                          key={item.id}
-                          className="nom nom18"
-                        >
-                          {item.r18}
-                        </p>
-                      );
-                    })}
-                  </td>
-                  <td>
-                    {raw.map((item) => {
-                      return (
-                        <p
-                          /* onClick={getRawRings} */
-                          key={item.id}
-                          className="nom nom12"
-                        >
-                          {item.r12}
-                        </p>
-                      );
-                    })}
-                  </td>
-                  <td>
-                    {raw.map((item) => {
-                      return (
-                        <p
-                          /* onClick={getRawRings} */
-                          key={item.id}
-                          className="nom nom12"
-                        >
-                          {item.r12s}
-                        </p>
-                      );
-                    })}
-                  </td>
-                  <td>
-                    {raw.map((item) => {
-                      return (
-                        <p
-                          /* onClick={getRawRings} */
-                          key={item.id}
-                          className="nom nom12"
-                        >
-                          {item.r12s1}
-                        </p>
-                      );
-                    })}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-
-</div>
-<style jsx>{`
-.container {
-}
-.nom {
-    color: pink
-}
-.nom:hover {
-    cursor: pointer
-}
-.nominell {
-    color: indianred
-}
-.nom12 {
-    color: red
-}
-.nom18 {
-    color: seagreen
-}
-td {
-    border: .5px solid #333;
-    padding: 2px;
-}
-`}
-</style>
-</>
-)
-}
-
-export default RawInputPanel
+export default RawInputPanel;
--- components/postoppsett/RingPanelComponent.tsx
+++ components/postoppsett/RingPanelComponent.tsx
@@ -1,63 +1,81 @@
-import React from 'react'
+import React from "react";
 
+const RingPanelComponent = ({
+  list,
+  setRingPanelNumber,
+  setUpdate,
+  update,
+  postInfo,
+  setPostInfo,
+}: any) => {
+  return (
+    <>
+      <p className="text-teal-100">Små ringer</p>
+      <div className="ringlist-container">
+        {list.small.map((item: any) => {
+          const smallRingsHandler = () => {
+            setPostInfo({
+              ...postInfo,
+              startRings: [...postInfo.startRings, { input: item }],
+            });
+            setUpdate(!update);
+          };
+          return (
+            <div
+              key={item._id}
+              onClick={() => setRingPanelNumber(item)}
+              className="ringvalue-container"
+            >
+              <p className="text-teal-100">{item}</p>
+            </div>
+          );
+        })}
+      </div>
+      <p className="text-teal-100">Store ringer</p>
+      <div className="ringlist-container">
+        {list.big.map((item: any) => {
+          return (
+            <div
+              key={item._id}
+              onClick={() => setRingPanelNumber(item)}
+              className="ringvalue-container"
+            >
+              <p className="text-teal-100">{item}</p>
+            </div>
+          );
+        })}
+      </div>
+      <p className="text-teal-100">Skims</p>
+      <div className="ringlist-container">
+        {list.shims.map((item: any) => {
+          return (
+            <div
+              key={item._id}
+              onClick={() => setRingPanelNumber(item)}
+              className="ringvalue-container"
+            >
+              <p className="text-teal-100">{item}</p>
+            </div>
+          );
+        })}
+      </div>
+      <style jsx>
+        {`
+          .ringlist-container {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(3rem, 1fr));
+            grid-gap: 5px;
+          }
+          .ringvalue-container {
+            border: 1px solid blue;
+            display: grid;
+            place-items: center;
+            width: 3rem;
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
-const RingPanelComponent = ({list, setRingPanelNumber, setUpdate, update, postInfo, setPostInfo}:any) => {
-
-   
-return (
-<>
-    <p className='text-teal-100'>Små ringer</p>
-<div className='ringlist-container'>
-   {list.small.map((item:any) => {
-    
-       const smallRingsHandler = () => {
-        setPostInfo({ ...postInfo, startRings: [ ...postInfo.startRings,{input: item}] });
-        setUpdate(!update)
-       }
-    return (
-        <div onClick={() => setRingPanelNumber(item)} className='ringvalue-container'>
-            <p className='text-teal-100'>{item}</p>
-        </div>
-    )
-   })}
-</div>
-    <p className='text-teal-100'>Store ringer</p>
-<div className='ringlist-container'>
-   {list.big.map((item:any) => {
-    return (
-        <div onClick={() => setRingPanelNumber(item)} className='ringvalue-container'>
-            <p className='text-teal-100'>{item}</p>
-        </div>
-    )
-   })}
-</div>
-    <p className='text-teal-100'>Skims</p>
-<div className='ringlist-container'>
-   {list.shims.map((item:any) => {
-    return (
-        <div onClick={() => setRingPanelNumber(item)} className='ringvalue-container'>
-            <p className='text-teal-100'>{item}</p>
-        </div>
-    )
-   })}
-</div>
-<style jsx>{`
-.ringlist-container {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(3rem, 1fr));
-    grid-gap: 5px
-}
-.ringvalue-container {
-    border: 1px solid blue;
-    display: grid;
-    place-items:center;
-    width: 3rem;
-
-}
-`}
-</style>
-</>
-)
-}
-
-export default RingPanelComponent
+export default RingPanelComponent;
