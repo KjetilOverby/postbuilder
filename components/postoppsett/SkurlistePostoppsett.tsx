5235fd7a3f61e22d07192c6d5584d4dabd6e98fb
KjetilOverby <kjeoever@msn.com>
2023-02-26 11:51:22 +0100

    color

--- components/postoppsett/SkurlistePostoppsett.tsx
+++ components/postoppsett/SkurlistePostoppsett.tsx
@@ -1,49 +1,89 @@
-import React from 'react'
+import React from "react";
 
-
-const SkurlistePostoppsett = ({skurliste, setSkurlisteInfo, searchTrigger, setSearchResultModal}:any) => {
-return (
-<>
-<div className="skurliste-container absolute">
-<table className="border table-auto w-auto border-spacing-2 shadow-md">
+const SkurlistePostoppsett = ({
+  skurliste,
+  setSkurlisteInfo,
+  searchTrigger,
+  setSearchResultModal,
+}: any) => {
+  return (
+    <>
+      <div className="skurliste-container absolute">
+        <table className="border table-auto w-auto border-spacing-2 shadow-md">
           <thead>
             <tr>
-              <th className="border border-slate-700  text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700  text-xs text-slate-600"
+                scope="col"
+              >
                 Treslag
               </th>
-              <th className="border border-slate-700  text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700  text-xs text-slate-600"
+                scope="col"
+              >
                 Kl
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Ant
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 m3
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Status
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Post
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 X-log
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 %
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Anm
               </th>
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Vs66
               </th>
-            
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Mkv
               </th>
-            
-              <th className="border border-slate-700 text-xs text-slate-600" scope="col">
+
+              <th
+                className="border border-slate-700 text-xs text-slate-600"
+                scope="col"
+              >
                 Blad
               </th>
             </tr>
@@ -52,7 +92,7 @@ return (
             skurliste.map((item: any) => {
               const skurlisteInfoHandler = () => {
                 setSkurlisteInfo(item);
-             
+
                 if (searchTrigger) {
                   setSearchResultModal(true);
                 }
@@ -72,7 +112,6 @@ return (
                     }`}
                   >
                     {item.treslag} {item.klType}
-                  
                   </td>
                   <td className="border border-slate-700 text-xs text-slate-400">
                     {item.klasse}
@@ -101,25 +140,20 @@ return (
                   </td>
                   <td className="border border-slate-700 text-xs text-slate-400 ">
                     <div className="two-input">
-                   <div>{item.anm}</div>
-                    
-                  <div>{item.anm2}</div>  
+                      <div>{item.anm}</div>
+
+                      <div>{item.anm2}</div>
                     </div>
                   </td>
                   <td className="border border-slate-700 text-xs text-slate-400">
-                    <div>
-
-                    {item.vs66}
-                    </div>
-                    <div className="text-red-600">
-                      {item.vs66Xtra}
-                    </div>
+                    <div>{item.vs66}</div>
+                    <div className="text-red-600">{item.vs66Xtra}</div>
                   </td>
-                  
+
                   <td className="border border-slate-700 text-xs text-slate-400">
                     {item.mkvBord}
                   </td>
-                
+
                   <td className="border border-slate-700 text-xs text-slate-400">
                     {item.blad}
                   </td>
@@ -128,21 +162,22 @@ return (
             })}
         </table>
       </div>
-<style jsx>{`
-.skurliste-container {
-  bottom: 10px;
-  right: 10px
-}
-.finished {
-  background: rgba(143, 143, 143, 0.2);
-}
-.running {
-  background: rgba(89, 226, 102, 0.3);
-}
-`}
-</style>
-</>
-)
-}
+      <style jsx>
+        {`
+          .skurliste-container {
+            bottom: 10px;
+            right: 10px;
+          }
+          .finished {
+            background: rgba(143, 143, 143, 0.2);
+          }
+          .running {
+            background: rgba(89, 226, 102, 0.3);
+          }
+        `}
+      </style>
+    </>
+  );
+};
 
-export default SkurlistePostoppsett
+export default SkurlistePostoppsett;
