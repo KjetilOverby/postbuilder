7253f84a8a60da1adce66a9e8095e6d213f228c7
KjetilOverby <kjeoever@msn.com>
2023-05-19 10:14:25 +0200

    color

--- components/lister/SortableItemList.js
+++ components/lister/SortableItemList.js
@@ -15,8 +15,8 @@ export function SortableItemList(props) {
           marginBottom: '.5rem',
           padding: '17px 10px',
           borderRadius: '5px',
-          background: 'var(--hover)',
-          color: 'var(--text)',
+          background: 'var(--table-bg)',
+          color: 'var(--primary-text)',
           display: 'flex',
         }}>
           <p style={{width: '4rem'}}>{props.treslag}</p>
@@ -24,8 +24,8 @@ export function SortableItemList(props) {
           <p style={{width: '7rem'}}>Ant: {props.ant} </p>
           <p style={{width: '7rem'}}>m3: {props.m3} </p>
           <p style={{width: '8rem'}}>status: {props.status} </p>
-          <p style={{width: '8rem'}}>xLog: {props.xLog} </p>
-          <p style={{width: '8rem'}}>%: {props.prosent} </p>
+          <p style={{width: '7rem'}}>xLog: {props.xLog} </p>
+          <p style={{width: '4rem'}}>%: {props.prosent} </p>
           <p>post: {props.post}x{props.breddePost} </p>
       </div>
     </div>
